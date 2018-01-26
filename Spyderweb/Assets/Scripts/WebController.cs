ec9a7548c1cd63c25e494afb4f78e7032e92d16f
Michael Elbert <derelbi@gmail.com>
2018-01-26 23:37:18 +0100

    added input handling with mouse

--- Spyderweb/Assets/Scripts/WebController.cs
+++ Spyderweb/Assets/Scripts/WebController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WebController : MonoBehaviour {
 
+    public WebNode webNodePrototype;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,9 +13,14 @@ public class WebController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) ||
+            Input.GetMouseButtonUp(0))
 		{
-			Debug.Log ("hello");
+            WebNode node = GameObject.Instantiate<WebNode>(webNodePrototype);
+            node.transform.parent = transform;
+            //node.transform.localPosition = Input.mousePosition;
+
+
 		}
 	
 	}
