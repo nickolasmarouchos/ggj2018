01148a017fb6309a474a92a9381d0e0ab03b7cdb
Nickolas Marouchos <nickolasmarouchos@nickolass-mbp.student.berlin.sae>
2018-01-27 23:47:07 +0100

    Add ui support for screens

--- Spyderweb/Assets/Scripts/GameStateManager.cs
+++ Spyderweb/Assets/Scripts/GameStateManager.cs
@@ -7,12 +7,17 @@ public class GameStateManager : MonoBehaviour {
     //public LoseScreen lose;
     //public WinScreen win;
     public WebController game;
+	public GameObject startScreen;
+	public GameObject endScreen;
+	public GameObject hud;
 
 	// Use this for initialization
 	void Start () {
-        game.gameObject.SetActive(false);
-
-        StartGame();
+		game.gameObject.SetActive(false);
+		startScreen.gameObject.SetActive (true);
+		endScreen.gameObject.SetActive (false);
+		hud.gameObject.SetActive (false);
+        //StartGame();
 	}
 	
 	// Update is called once per frame
@@ -20,8 +25,19 @@ public class GameStateManager : MonoBehaviour {
 		
 	}
 
-    void StartGame()
+	public void EndGame()
+	{
+		hud.gameObject.SetActive (false);
+		endScreen.SetActive (true);
+		game.gameObject.SetActive(false);
+	}
+
+    public void StartGame()
     {
-        game.gameObject.SetActive(true);
+		startScreen.SetActive (false);
+		endScreen.SetActive (false);
+		hud.gameObject.SetActive (true);
+		game.gameObject.SetActive(true);
+
     }
 }
